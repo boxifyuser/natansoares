6b445f06e77850f3eb4678901be9adbb0ad588bb
Boxify <contato@boxify.com.br>
2026-02-04 14:31:58 -0300

    fix: corrigir build - excluir diretÃ³rios externos do TypeScript
    - Adicionar exclusÃµes especÃ­ficas no tsconfig.json
    - Criar .vercelignore para ignorar diretÃ³rios externos
    - Configurar webpack no next.config.js para ignorar arquivos fora do projeto
    - Resolver erro de compilaÃ§Ã£o de arquivos em Documents/Unique/

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  // Ignorar diretórios fora do projeto
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  // Garantir que apenas arquivos do diretório atual sejam processados
+  webpack: (config, { isServer }) => {
+    // Ignorar arquivos fora do diretório do projeto
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/node_modules/**',
+        '**/.next/**',
+        '**/Documents/**',
+        '**/Unique/**',
+        '**/lp/**',
+      ],
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
